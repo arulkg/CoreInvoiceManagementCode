b05496095f1e42521e4a4490e97e520fcc9a6a5f
arulkg <arulkg@yahoo.com>
2024-09-15 21:16:35 -0500

    Core initial code added

---
+++ InvoiceManagementWebApiCore/Model/Category.cs
@@ -0,0 +1,14 @@
+﻿namespace InvoiceManagementWebApiCore.Model
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public float MinimumStock { get; set; }
+        public float MaximumStock { get; set; }
+        public float MinimumRate { get; set; }
+        public float MaximumRate { get; set; }
+
+    }
+}
---
+++ InvoiceManagementWebApiCore/Model/IMG_CoreContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace InvoiceManagementWebApiCore.Model
+{
+    public class IMG_CoreContext:DbContext
+    {
+
+        public IMG_CoreContext(DbContextOptions<IMG_CoreContext> options):base(options) { }
+
+        public DbSet<Category> Categories { get; set; } 
+
+    }
+}
